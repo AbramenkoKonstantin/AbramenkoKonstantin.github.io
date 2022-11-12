5271949c63c81998a5fed56be90a171c028fc8a5
AbramenkoKonstantin <kostaabramenko695@gmail.com>
2022-11-12 16:12:08 +0700

    lab7

---
+++ scripts.js
@@ -0,0 +1,30 @@
+//3
+console.log("Hello");
+
+//4
+let a = 5;
+let b = 2;
+console.log("first number =", a);
+console.log("second number =", b);
+
+//5
+console.log("sum of numbers =", a+b);
+
+//6
+let str = "Hello";
+console.log("String to lower:", str.toLowerCase());
+console.log("String to upper:", str.toUpperCase());
+
+//7
+console.log("Сумма чисел =", a + b)
+console.log("Разность чисел =", a - b)
+console.log("Произведение чисел =", a * b)
+console.log("Деление чисел =", a / b)
+console.log("Возведение в степень =", a ** b)
+console.log("Остаток от деления =", a % b)
+
+//8
+//Вывести рандомное число
+console.log("Рандомное число от 0 до 10:", Math.round(Math.random(10) * 10))
+//Сделать из строки массив
+console.log(str.split(""))
